703fe724a222e9e2ef1d3d6c0ef139a7ffa43e15
Risa <42893634+HopeEternal@users.noreply.github.com>
2019-09-20 19:57:11 -0600

    Axios install and JSONPlaceholder setup to fetch faux todo's

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,50 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Header from './components/Layout/header.vue';
+import Header from './components/Layout/Header.vue';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
     return {
       todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
       ]
     }
   },
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !==id);
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+
+      
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
 }
 </script>
@@ -55,4 +60,18 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
   }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  btn:hover {
+    background: #666;
+  }
+
 </style>
